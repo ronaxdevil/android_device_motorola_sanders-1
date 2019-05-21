840a38851e4508e508716ada365a3d42cb2da428
Jorg3Lucas <jorgelucas.sd@gmail.com>
2019-05-21 15:43:06 +0200

    HydrusOS Bringup
    Signed-off-by: Jorg3Lucas <jorgelucas.sd@gmail.com>

--- hydrus_sanders.mk
+++ hydrus_sanders.mk
@@ -25,7 +25,7 @@ $(call inherit-product, device/motorola/sanders/device.mk)
 $(call inherit-product, vendor/motorola/sanders/sanders-vendor.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/hydrus/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
@@ -34,7 +34,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := sanders
-PRODUCT_NAME := lineage_sanders
+PRODUCT_NAME := hydrus_sanders
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
 
